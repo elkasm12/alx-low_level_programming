929cb9182ea9be94d6377ede563982e33590f43a
elkasm12 <a.elkasmi.zakariyae@gmail.com>
2023-03-16 16:35:49 -0700

    printf, hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  * Return: always 0
  */
 int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art\n");
+{f
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
