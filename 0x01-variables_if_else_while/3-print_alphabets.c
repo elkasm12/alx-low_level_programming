b0aef3953c2e7d1988faf0c4faad8113ebd5fa52
elkasm12 <a.elkasmi.zakariyae@gmail.com>
2023-03-17 15:39:09 -0700

    alphabet iworcase uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char i, j;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for (j = 'A'; j <= 'Z' ; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
