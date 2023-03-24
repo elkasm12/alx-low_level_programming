e24f528c75d01d103e93ff11dd8b890abc270a53
elkasm12 <a.elkasmi.zakariyae@gmail.com>
2023-03-24 03:45:39 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print from n to 98 ascendly or descendly
+ *
+ * @n: parametr to be entred
+ *
+ * Return: always 0
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n >= 0 && n <= 98)
+	{
+		for (i = n ; i <= 98 ; i++)
+			printf("%d, ", i);
+		printf("\n");
+	}
+	else if (n < 0)
+	{
+		for (i = n ; i <= 98 ; i++)
+			printf("%d, ", i);
+		printf("\n");
+	}
+	else if (n > 98)
+	{
+		for (i = n ; i >= 98 ; i--)
+			printf("%d, ", i);
+		printf("\n");
+	}
+}
