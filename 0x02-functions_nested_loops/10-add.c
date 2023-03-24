a958d3418686581359d70bbca98ed0aae75d8541
elkasm12 <a.elkasmi.zakariyae@gmail.com>
2023-03-24 03:14:13 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ *
+ * @a: first parameter to be summed
+ *
+ * @b: second parameter to summed
+ *
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
