b6210c4075ec8b2cb8d546be5eaad75ba417d146
elkasm12 <a.elkasmi.zakariyae@gmail.com>
2023-03-29 09:57:23 -0700

    cpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,10 +10,15 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i, j=0;
 
 	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+	{
 		dest[i] = src[i];
+		j++;
+	}
+	for (i = j ; i < n ; i++)
+		dest[i] = '\0';
 	return (dest);
 }
 
