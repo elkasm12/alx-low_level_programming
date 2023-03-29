6a98f3bed58feb133c8247c65a71ecba5340167b
elkasm12 <a.elkasmi.zakariyae@gmail.com>
2023-03-29 11:02:59 -0700

    array reverse

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,10 +10,10 @@ void reverse_array(int *a, int n)
 {
 	int i, j;
 
-	for (i = 0 ; i < n /2 ; i++)
+	for (i = 0 ; i < n / 2 ; i++)
 	{
 		j = a[i];
 		a[i] = a[n - 1 - i];
-		a[n - i - 1 ] = j;
+		a[n - i - 1] = j;
 	}
 }
