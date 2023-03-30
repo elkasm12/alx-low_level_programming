537b432e9a64a4d966f804fe7570a4f2d930b587
elkasm12 <a.elkasmi.zakariyae@gmail.com>
2023-03-30 05:43:00 -0700

    cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,25 +12,33 @@ char *cap_string(char *str)
 
 	for (i = 0 ; str[i] != '\0' ; i++)
 	{
-		if (str[i] == ' '
-		|| str[i] == '.'
-		|| str[i] == '\n'
-		|| str[i] == '\t'
-		|| str[i] == ','
-		|| str[i] == ';'
-		|| str[i] == ':'
-		|| str[i] == '{'
-		|| str[i] == '?'
-		|| str[i] == '}'
-		|| str[i] == '['
-		|| str[i] == ']'
-		|| str[i] == '!'
-		|| str[i] == '"'
-		|| str[i] == '('
-		|| str[i] == ')')
+		if (i != 0)
 		{
-			if (str[i + 1] >= 97 && str[i + 1] <= 122)
-				str[i + 1] = str[i + 1] - 32;
+			if (str[i] == ' '
+			|| str[i] == '.'
+			|| str[i] == '\n'
+			|| str[i] == '\t'
+			|| str[i] == ','
+			|| str[i] == ';'
+			|| str[i] == ':'
+			|| str[i] == '{'
+			|| str[i] == '?'
+			|| str[i] == '}'
+			|| str[i] == '['
+			|| str[i] == ']'
+			|| str[i] == '!'
+			|| str[i] == '"'
+			|| str[i] == '('
+			|| str[i] == ')')
+			{
+				if (str[i + 1] >= 97 && str[i + 1] <= 122)
+					str[i + 1] = str[i + 1] - 32;
+			}
+		}
+		else
+		{
+			if (str[i] >= 97 && str[i] <= 122)
+				str[i] = str[i] - 32;
 		}
 	}
 	return (str);
