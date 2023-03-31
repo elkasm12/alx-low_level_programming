0f968ee93d4db2c9bd3d464abb785bf4fb04e5de
elkasm12 <a.elkasmi.zakariyae@gmail.com>
2023-03-31 01:01:05 -0700

    integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+void print_number(int n)
+{
+	unsigned int m;
+
+	m = n;
+	if ( n < 0)
+	{
+		_putchar('-');
+		m = - n;
+	}
+	if (m / 10 != 0)
+		print_number(m / 10);
+	_putchar((m % 10) + '0');
+
+}
