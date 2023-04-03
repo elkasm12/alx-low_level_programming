0f7708c1df6fd9e3717df5994ac1db9bfccfa4f0
elkasm12 <a.elkasmi.zakariyae@gmail.com>
2023-04-03 08:46:03 -0700

    strshr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - find charcter in memeory
+ * @s: string to look in
+ * @c: char to look for
+ * Return: s+i address and Null if there isn't
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		if ( s[i] == c)
+			return (s + i);
+	}
+	return (NULL);
+}
