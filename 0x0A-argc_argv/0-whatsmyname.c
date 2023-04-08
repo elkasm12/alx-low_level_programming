2ba1ee198f69f23bd899a10132fb90d9c7c99266
elkasm12 <a.elkasmi.zakariyae@gmail.com>
2023-04-08 08:33:36 -0700

    argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - the main function 
+ * @argc: number of argument
+ * @argv: copy of string of these argument
+ * Return: always 0
+ */
+
+int  main(int argc __attribute__ ((unused)), char const *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
