8cf6c86ae86960a5c20680fae47a33d6bbdd3df3
elkasm12 <a.elkasmi.zakariyae@gmail.com>
2023-04-08 10:05:10 -0700

    tr

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,11 +13,10 @@ int main(int argc, char *argv[])
 	{
 		int i;
 		int mul = 1;
+
 		for (i = 1; i < argc; i++)
-		{
 			mul *= atoi(argv[i]);
-		}	
-			printf("%d\n", mul);
+		printf("%d\n", mul);
 	}
 	else
 		printf("error\n");
