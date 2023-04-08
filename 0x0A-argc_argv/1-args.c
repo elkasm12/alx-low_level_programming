1b929e4c1ef18cb3660c30723e7cfdafac444a82
elkasm12 <a.elkasmi.zakariyae@gmail.com>
2023-04-08 08:46:42 -0700

    nargs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * @argc: number of argument
+ * @argv: array of argument
+ * Return: 0
+ */
+
+int main(int argc, char *argv[] __attribute__ ((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
