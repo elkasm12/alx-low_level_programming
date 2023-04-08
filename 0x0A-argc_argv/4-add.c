c88e96cc4f93696e39b6e8a4bc91d340ae094611
elkasm12 <a.elkasmi.zakariyae@gmail.com>
2023-04-08 16:04:56 -0700

    sum-args

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * main - this is the main function
+ * @argc: number of argument
+ * @argv: array of argument
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+	{
+		int i, j, sum = 0;
+	/*	char *str;*/
+
+		for (i = 1; i < argc; i++)
+		{
+			/*length = strlen(str);
+			str = argv[i];*/
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (!((argv[i][j] >= '0' && argv[i][j] <= '9') || argv[i][j] == '-'))
+				{
+					printf("Error\n");
+					return (1);
+				}
+				else
+					sum = sum + atoi(argv[i]);
+			}
+		}
+		printf("%d\n", sum);
+	}
+	else
+		printf("0\n");
+	return (0);
+}
