2009033d3a1b7dff33f21a542314c5cf3c4c8dbd
elkasm12 <a.elkasmi.zakariyae@gmail.com>
2023-04-09 15:12:37 -0700

    add-args

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,37 +1,34 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
 
 /**
- * main - this is the main function
- * @argc: number of argument
- * @argv: array of argument
- * Return: 0
+ * main - adds positive numbers
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: int
  */
-
 int main(int argc, char *argv[])
 {
-	if (argc > 1)
-	{
-		int i, j, sum = 0;
+	int i, j, sum = 0;
 
-		for (i = 1; i < argc; i++)
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			for (j = 0; argv[i][j] != '\0'; j++)
+			if (!((argv[i][j] >= '0' && argv[i][j] <= '9') || argv[i][j] == '-'))
 			{
-				if (!((argv[i][j] >= '0' && argv[i][j] <= '9') || argv[i][j] == '-'))
-				{
-					printf("Error\n");
-					return (1);
-				}
-				else
-					sum = sum + atoi(argv[i]);
+				printf("Error\n");
+				return (1);
 			}
 		}
-		printf("%d\n", sum);
+		sum += atoi(argv[i]);
 	}
-	else
-		printf("0\n");
+	printf("%d\n", sum);
 	return (0);
 }
