04e6efbf687a268636d8b93e8648b3f94ed805f7
elkasm12 <a.elkasmi.zakariyae@gmail.com>
2023-04-11 11:12:22 -0700

    grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * alloc_grid - allocate dimensional array
+ * @width: width of array
+ * @height: row of array
+ * Return arr
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j, **arr;
+
+	arr = malloc(height * sizeof(int));
+	if (height <= 0 || width <= 0 || arr == 0)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = malloc(width * sizeof(int));
+		if (arr[i] == 0)
+		{
+			while (i--)
+				free(arr[i]);
+			free(arr);
+		}
+		for (j = 0; j < width; j++)
+			arr[i][j] = 0;
+	}
+	return (arr);
+}
