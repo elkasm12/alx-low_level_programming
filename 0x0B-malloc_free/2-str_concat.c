6f7c4927d662ee55d381c6235f8617db1aac34b6
elkasm12 <a.elkasmi.zakariyae@gmail.com>
2023-04-11 10:21:32 -0700

    concate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,9 +13,9 @@ char *str_concat(char *s1, char *s2)
 	char *s;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "\0";
 	if (s2 == NULL)
-		s2 = '\0';
+		s2 = "\0";
 	for (i = 0; s1[i] != '\0'; i++)
 		c1++;
 	for (i = 0; s2[i] != '\0'; i++)
