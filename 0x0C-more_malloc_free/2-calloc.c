575a9a603d028b459a79041cf1d94fa5f9a11aa9
elkasm12 <a.elkasmi.zakariyae@gmail.com>
2023-04-13 11:24:49 -0700

    colloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,25 +1,5 @@
 #include "main.h"
 /**
- * _memset - put a constant into a block of memory
- * @s: memory block
- * @b: constant to be stored
- * @n: number of repition
- * Return: s
- */
-
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int i;
-	char *str = s;
-
-	for (i = 0 ; i < n ; i++)
-	{
-		*s++ = b;
-	}
-	return (str);
-}
-
-/**
  * _calloc - calloc implement
  * @nmemb: number of memory byte
  * @size: var size
@@ -32,10 +12,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (size == 0 || nmemb == 0)
 		return (NULL);
-	ml = malloc(sizeof(int) * nmemb + 1);
+	ml = malloc(sizeof(int) * nmemb);
 	if (ml == 0)
 		return (NULL);
-	_memset(ml, 0, sizeof(int) * nmemb);
+	for (i = 0; i < (nmemb * size); i++)
+		ml[i] = 0;
 	return (ml);
 }
 
