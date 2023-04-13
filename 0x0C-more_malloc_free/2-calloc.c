b1c710de7bbd187812c8ceb4efd57a4f7a123dfa
elkasm12 <a.elkasmi.zakariyae@gmail.com>
2023-04-13 11:32:06 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,15 +8,16 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ml;
+	char *mal;
+	unsigned int i;
 
 	if (size == 0 || nmemb == 0)
 		return (NULL);
-	ml = malloc(sizeof(int) * nmemb);
-	if (ml == 0)
+	mal = malloc(sizeof(int) * nmemb);
+	if (mal == 0)
 		return (NULL);
 	for (i = 0; i < (nmemb * size); i++)
-		ml[i] = 0;
-	return (ml);
+		mal[i] = 0;
+	return (mal);
 }
 
