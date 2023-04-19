4e7fff8b13d38ea4d39f434e2841ced554be6b52
elkasm12 <a.elkasmi.zakariyae@gmail.com>
2023-04-19 09:34:56 -0700

    op_func

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,20 +9,20 @@
 
 int main(int argc, char **argv)
 {
-	int (*op_func)(int, int), a, b;
-
+	int  a, b;
+	char *n;
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 	a = atoi(argv[1]);
-	b =atoi(argv[3]);
-	op_func = get_op_func(argv[2]);
-	if (!op_func)
+	b = atoi(argv[3]);
+	n = argv[2];
+	if (get_op_func(n) == NULL || argv[2][1])
 		printf("Error\n"), exit(99);
 	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
 		printf("Error\n"), exit(100);
-	printf ("%d\n", op_func(a, b));
+	printf("%d\n", get_op_func(n)(a, b));
 	return (0);
 }
