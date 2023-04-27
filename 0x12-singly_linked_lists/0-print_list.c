e0a9863cc0abac773dee4a22024a5944b6a2dfb4
elkasm12 <a.elkasmi.zakariyae@gmail.com>
2023-04-27 08:01:20 -0700

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+
+/**
+ * _strlen - count string
+ * @s: string to be counted
+ * Return: count
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int count = 0;
+
+	if (!s)
+		return (0);
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+		count++;
+	return (count);
+}
+
+/**
+ * print_list - print all elements of a list
+ * @h: pointer to the head
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
