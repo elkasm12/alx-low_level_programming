c171a378e81fecbf1a8deb956fdb76d21ec52790
elkasm12 <a.elkasmi.zakariyae@gmail.com>
2023-04-27 08:34:00 -0700

    add elem in the beginning

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_h = malloc(sizeof(list_t));
+
+	if (!head || !new_h)
+		return (NULL);
+	if (str)
+	{
+		new_h->str = strdup(str);
+		if (!new_h->str)
+		{
+			free(new_h);
+			return (NULL);
+		}
+		new_h->len = strlen(new_h->str);
+	}
+		new_h->next = *head;
+		*head = new_h;
+		return (new_h);
+}
+
