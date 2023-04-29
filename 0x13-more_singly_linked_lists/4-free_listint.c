7dc30998a930b4d2d61bcfa27b570e7b56101474
elkasm12 <a.elkasmi.zakariyae@gmail.com>
2023-04-29 16:46:55 -0700

    free

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint - free list 
+ * @head: adress of the head
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *node, *node_next;
+
+	if (!head)
+		return;
+	node = head;
+	while (node)
+	{
+		node_next = node->next;
+		free(node);
+		node = node_next;
+	}
+}
