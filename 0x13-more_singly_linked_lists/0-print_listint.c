3640ffcae9a5163cc66ea4600127dbb0da66bce6
elkasm12 <a.elkasmi.zakariyae@gmail.com>
2023-04-29 16:53:46 -0700

    print

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,20 +1,20 @@
 #include "lists.h"
+
 /**
  * print_listint - print number of node
  * @h: head of node
  * Return: i number of nodes
  */
+
 size_t print_listint(const listint_t *h)
 {
 	size_t i = 0;
 
-	if (!h)
-		return (0);
 	while (h)
 	{
 		printf("%d\n", h->n);
-		h = h->next;
 		i++;
+		h = h->next;
 	}
 	return (i);
 }
