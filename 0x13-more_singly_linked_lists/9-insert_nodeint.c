8b075e8f1fa0ab2810490fa1f7ff6a44c77b211d
elkasm12 <a.elkasmi.zakariyae@gmail.com>
2023-05-02 11:13:33 -0700

    insert node in index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -4,7 +4,8 @@
  * insert_nodeint_at_index - insert a  node at index
  * @head: head of node
  * @idx: index to insert a node at
- * @n: integer
+ * @n: integer]
+ * Return: address of new node
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
@@ -37,4 +38,4 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	insert->next = temp->next;
 	temp->next = insert;
 	return (insert);
-}	
+}
