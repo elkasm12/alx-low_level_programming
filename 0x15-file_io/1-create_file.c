27552e025c629fe0fb19dcaea59953d9021b7e94
elkasm12 <a.elkasmi.zakariyae@gmail.com>
2023-05-09 07:30:12 -0700

    create file and put text on it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,11 +29,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC , 0600);
+	fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fp == -1)
 		return (-1);
 	if (length)
-		str = write (fp, text_content, length);
-	close (fp);
+		str = write(fp, text_content, length);
+	close(fp);
 	return (str == length ? 1 : -1);
 }
