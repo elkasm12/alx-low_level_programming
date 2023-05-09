e0020ade45fb7a449aa22a45e3a46e182e45a78d
elkasm12 <a.elkasmi.zakariyae@gmail.com>
2023-05-09 08:07:42 -0700

    create file and append text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,6 +11,8 @@ int _strlen(char *s)
 	int i;
 	int count = 0;
 
+	if (!s)
+		return (0);
 	for (i = 0 ; s[i] != '\0' ; i++)
 		count++;
 	return (count);
@@ -25,7 +27,15 @@ int _strlen(char *s)
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fp;
-	size_t str, length = _strlen(text_content);
+	size_t str = 0, length = _strlen(text_content);
 
-
+	if (!filename)
+		return (-1);
+	fp = open(filename, O_WRONLY | O_APPEND);
+	if (fp == -1)
+		return (-1);
+	if (length)
+		str = write(fp, text_content, length);
+	close(fp);
+	return (str == length ? 1 : -1);
 }
