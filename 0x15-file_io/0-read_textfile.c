d0c879f35296bb7ac6798f41df92ee7ba2c6634e
elkasm12 <a.elkasmi.zakariyae@gmail.com>
2023-05-09 05:15:34 -0700

    read a file and print in POSIX

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * read_textfile - read a text from a file
+ * @filename: file to read from
+ * @letters: number of bytes to read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fp;
+	size_t str;
+	char buf[READ_BUF_SIZE * 8];
+	
+	if (!filename || !letters)
+		return (0);
+	fp = open(filename, O_RDONLY);
+	if (fp == -1)
+		return (0);
+	str = read(fp, &buf[0], letters);
+	str = write(STDOUT_FILENO, &buf[0], str);
+	close(fp);
+	return (str);
+}
